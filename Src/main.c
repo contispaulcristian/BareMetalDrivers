bcf1fa7636a42e9d5a3ec67b083fac45c0f9ae2a
Paul Contis <contispaulcristian@gmail.com>
2024-04-05 16:29:18 +0300

    Empty project

---
+++ Src/main.c
@@ -0,0 +1,29 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Paul Contis
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 Paul Contis.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+
+
+int main(void)
+{
+    /* Loop forever */
+	while(1)
+	{
+
+	}
+}
