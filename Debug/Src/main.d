b0dd9cbac546365781b5c7cc1274ceff5399fc5d
Paul Contis <contispaulcristian@gmail.com>
2024-04-06 11:38:54 +0300

    Started drivers layer implementation.

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,1 +1,7 @@
-Src/main.o: ../Src/main.c
+Src/main.o: ../Src/main.c \
+ D:/STM32_Workspace/BareMetalDrivers/Drivers/Inc/stm32f446xx.h \
+ D:/STM32_Workspace/BareMetalDrivers/Drivers/Inc/stm32f446xx_gpio_driver.h \
+ D:/STM32_Workspace/BareMetalDrivers/Drivers/Inc/stm32f446xx.h
+D:/STM32_Workspace/BareMetalDrivers/Drivers/Inc/stm32f446xx.h:
+D:/STM32_Workspace/BareMetalDrivers/Drivers/Inc/stm32f446xx_gpio_driver.h:
+D:/STM32_Workspace/BareMetalDrivers/Drivers/Inc/stm32f446xx.h:
