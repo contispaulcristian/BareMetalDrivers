aa97390cebbfcfccc3b5817837e8586de09fcacf
Paul Contis <contispaulcristian@gmail.com>
2024-04-19 16:12:59 +0300

    SPI test application update

--- Src/main.c
+++ Src/main.c
@@ -21,11 +21,28 @@
 /*************************************************************************************************************************************************/
 #define LOW				0
 #define BTN_PRESSED		LOW
+
+/* command codes */
+#define COMMAND_LED_CTRL		0x50
+#define COMMAND_SENSOR_READ		0x51
+#define COMMAND_LED_READ		0x52
+#define COMMAND_PRINT			0x53
+#define COMMAND_ID_READ			0x51
+
+#define LED_ON					1
+#define LED_OFF					0
+
+/* arduino analog pins */
+#define ANALOG_PIN0				0
+
+/* arduino led */
+#define LED_PIN					9
 /*************************************************************************************************************************************************/
 void delay(void);
 void SPI2_GPIOInits(void);
 void SPI2_Inits(void);
 void GPIO_ButtonInit(void);
+uint8_t SPI_VerifyResponse(uint8_t ackbyte);
 /*************************************************************************************************************************************************/
 
 /*	This is a simple application to test SPI communication using polling mode.
@@ -37,15 +54,19 @@ void GPIO_ButtonInit(void);
 
 int main(void)
 {
-	char user_data[]= "Hello World";
-	uint8_t dataLength = strlen(user_data);
-
+	//char user_data[]= "Hello World";
+	//uint8_t dataLength = strlen(user_data);
+	uint8_t dummy_write = 0xff;
+	uint8_t cmd_code = COMMAND_LED_CTRL;
+	uint8_t ackbyte;
+	uint8_t args[2];
+	uint8_t dummy_read;
 
 	GPIO_ButtonInit();
 
 	SPI2_GPIOInits();	/* This function is used to initialize the GPIO pins to behave as SPI2 pins*/
 
-	SPI2_Inits();
+	SPI2_Inits();		/* This function is used to initialize the SPI2 peripheral parameters */
 
 	SPI_SSOEConfig(SPI2,  ENABLE);
 
@@ -58,11 +79,25 @@ int main(void)
 		/* enable the SPI2 peripheral */
 		SPI_PeripheralControl(SPI2, ENABLE);
 
-		/* first send length information*/
-		SPI_SendData(SPI2, &dataLength, 1);
+		/* CMD_LED_CTRL <pin no(1)> <value(1)> */
+		SPI_SendData(SPI2, &cmd_code, 1);
 
-		/* send data */
-		SPI_SendData(SPI2, (uint8_t*)user_data, strlen(user_data));
+		/* do dummy read to clear off the RXNE*/
+		SPI_ReceiveData(SPI2, &dummy_read, 1);
+
+		/* send some dummy bits (1byte) to fetch the response from the slave. */
+		SPI_SendData(SPI2, &dummy_write, 1);
+
+		/* read the ack byte received */
+		SPI_ReceiveData(SPI2, &ackbyte, 1);
+
+		if ( SPI_VerifyResponse(ackbyte) )
+		{
+			/* send arguments */
+			args[0] = LED_PIN;
+			args[1] = LED_ON;
+			SPI_SendData(SPI2, args, 2);
+		}
 
 		while( SPI_GetFlagStatus(SPI2, SPI_BUSY_FLAG) );
 
@@ -99,8 +134,8 @@ void SPI2_GPIOInits(void)
 	GPIO_Init(&SPIPins);
 
 	/* MISO */
-	//SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
-	//GPIO_Init(&SPIPins);
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	GPIO_Init(&SPIPins);
 
 	/* NSS */
 	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
@@ -138,6 +173,18 @@ void GPIO_ButtonInit(void)
 
 		GPIO_Init(&GpioBtn);
 }
+
+/*************************************************************************************************************************************************/
+uint8_t SPI_VerifyResponse(uint8_t ackbyte)
+{
+	if( ackbyte == 0xF5)
+	{
+		/* ack */
+		return 1;
+	}
+	return 0;
+}
+
 
 
 
